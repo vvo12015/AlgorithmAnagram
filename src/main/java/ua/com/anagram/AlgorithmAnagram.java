defb0609e4ad7e9b603108ca3ffa25e22c0b97e8
B1R1 <roman.burko@gmail.com>
2016-04-18 20:57:57 +0300

    Подправил AlgorithmAnagram и  Prepare классы

--- src/main/java/ua/com/anagram/AlgorithmAnagram.java
+++ src/main/java/ua/com/anagram/AlgorithmAnagram.java
@@ -12,19 +12,29 @@ public class AlgorithmAnagram implements Algorithm {
 
     @Override
     public Map<String, List<String>> execute(List<String> listToWorkWith) {
+
+        final Map<String, List<String>> mapTemp = new HashMap<>();
+
         if (listToWorkWith == null || listToWorkWith.isEmpty()) {
             return map;
         }
+
         for (String index : listToWorkWith) {
             String sortedWord = sortWord(index);
-            List<String> anagrams = map.get(sortedWord);
+            List<String> anagrams = mapTemp.get(sortedWord);
             if (anagrams == null) {
                 List<String> anagramsNew = new ArrayList<>();
                 anagramsNew.add(index);
-                map.put(sortedWord, anagramsNew);
+                mapTemp.put(sortedWord, anagramsNew);
             } else {
-                anagrams.add(index);
+                if (!anagrams.contains(index)) {
+                    anagrams.add(index);
+                }
             }
+        }
+
+        for (Map.Entry<String, List<String>> entry : mapTemp.entrySet()) {
+            map.put(entry.getValue().get(0), entry.getValue());
         }
 
         return map;
@@ -35,31 +45,4 @@ public class AlgorithmAnagram implements Algorithm {
         Arrays.sort(a);
         return new String(a);
     }
-
-
-    //    public static void main(String[] args) {
-    //
-    //        List<String> list = new ArrayList<>();
-    //        list.add("кат");
-    //        list.add("мама");
-    //        list.add("акт");
-    //        list.add("рама");
-    //        list.add("шина");
-    //        list.add("амам");
-    //        list.add("ниша");
-    //
-    //        System.out.print("Text/ list of words: ");
-    //        for (String s : list) {
-    //            System.out.print(s + " ");
-    //        }
-    //        System.out.println();
-    //
-    //        AlgorithmAnagram obj = new AlgorithmAnagram();
-    //        List<String> listTest = obj.execute(list);
-    //        System.out.println("\nAnagrams: ");
-    //        for (String s : listTest) {
-    //            System.out.print(s + " ");
-    //        }
-    //    }
 }
-
