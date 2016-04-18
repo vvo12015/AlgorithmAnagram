2734cffde3a61ce5b3a178453ea64884b4201c6d
shtroodel <yurok.andrey@gmail.com>
2016-04-18 23:34:22 +0300

    created and realised checkUkrainian

--- src/main/java/ua/com/anagram/Prepare.java
+++ src/main/java/ua/com/anagram/Prepare.java
@@ -44,12 +44,35 @@ class Prepare {
 
     public static boolean checkRussian(String s){
         final static int FIRST_RUSSIAN_SYMBOL = 224;
-        final static int LAST_RUSSIAN_SYMBOL = 255;
         final static int RUSSIAN_SYMBOL_YO = 184;
 
         s = s.toLowerCase();
         for (int i = 0; i < s.length(); i++) {
             if ((int)s[i] < FIRST_RUSSIAN_SYMBOL && (int)s[i] != RUSSIAN_SYMBOL_YO) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkUkrainian(String s){
+        final static int FIRST_RUSSIAN_SYMBOL = 224;
+        final static int TVERDYI_ZNAK = 184;
+        final static int OPERACIA_bl = 251;
+        final static int REVERSE_E = 253;
+        final static int UKRAINIAN_G = 180;
+        final static int UKRAINIAN_E = 186;
+        final static int UKRAINIAN_YI = 191;
+
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            if ((int)s[i] < FIRST_RUSSIAN_SYMBOL
+                    && (int)s[i] != UKRAINIAN_E
+                    && (int)s[i] != UKRAINIAN_G
+                    && (int)s[i] != UKRAINIAN_YI
+                    || (int)s[i] == TVERDYI_ZNAK
+                    || (int)s[i] == OPERACIA_bl
+                    || (int)s[i] == REVERSE_E) {
                 return false;
             }
         }
