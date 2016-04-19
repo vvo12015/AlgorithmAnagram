52ab0df8ed663d39b03a664947be43fac8182354
B1R1 <roman.burko@gmail.com>
2016-04-19 11:40:39 +0300

    Добавил проверку пунктуации в  Prepare, подправил тест.

--- src/main/java/ua/com/anagram/Prepare.java
+++ src/main/java/ua/com/anagram/Prepare.java
@@ -1,6 +1,5 @@
 package ua.com.anagram;
 
-import java.net.FileNameMap;
 import java.util.*;
 
 class Prepare {
@@ -9,12 +8,14 @@ class Prepare {
 
         List<String> preparedList = new ArrayList<>();
 
-        for (String word: list) {
-            if (isWord(word) && !word.equals("")) {
-                preparedList.add(word.toLowerCase());
+        for (int i = 0; i < list.size(); i++) {
+
+            list.set(i, list.get(i).replaceAll("[\\p{Punct} ]", ""));
+
+            if (isWord(list.get(i)) && !list.get(i).equals("")) {
+                preparedList.add(list.get(i).toLowerCase());
             }
         }
-
         return preparedList;
     }
 
@@ -29,58 +30,4 @@ class Prepare {
         }
         return true;
     }
-
-    public static boolean checkEnglish(String s){
-        final int FIRST_ENGLISH_SYMBOL = 97;
-        final int LAST_ENGLISH_SYMBOL = 122;
-
-        s = s.toLowerCase();
-        for (int i = 0; i < s.length(); i++) {
-            int ch = (int)s.charAt(i);
-            if (ch < FIRST_ENGLISH_SYMBOL || ch > LAST_ENGLISH_SYMBOL) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkRussian(String s){
-        final int FIRST_RUSSIAN_SYMBOL = 224;
-        final int RUSSIAN_SYMBOL_YO = 184;
-
-        s = s.toLowerCase();
-        for (int i = 0; i < s.length(); i++) {
-            int ch = (int)s.charAt(i);
-            if (ch < FIRST_RUSSIAN_SYMBOL && ch != RUSSIAN_SYMBOL_YO) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean checkUkrainian(String s){
-        final int FIRST_RUSSIAN_SYMBOL = 224;
-        final int TVERDYI_ZNAK = 184;
-        final int OPERACIA_bl = 251;
-        final int REVERSE_E = 253;
-        final int UKRAINIAN_G = 180;
-        final int UKRAINIAN_E = 186;
-        final int UKRAINIAN_YI = 191;
-
-        s = s.toLowerCase();
-        for (int i = 0; i < s.length(); i++) {
-            int ch = (int)s.charAt(i);
-            if (ch < FIRST_RUSSIAN_SYMBOL
-                    && ch != UKRAINIAN_E
-                    && ch != UKRAINIAN_G
-                    && ch != UKRAINIAN_YI
-                    || ch == TVERDYI_ZNAK
-                    || ch == OPERACIA_bl
-                    || ch == REVERSE_E) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
--- src/test/java/ua/com/anagram/PrepareTest.java
+++ src/test/java/ua/com/anagram/PrepareTest.java
@@ -1,35 +1,29 @@
 package ua.com.anagram;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class PrepareTest {
 
-    private static Prepare prepareObject;
     private static final List<String> list = new ArrayList<>();
 
     static {
         list.add("brother");
         list.add("brother123");
-        list.add("brother.,");
-        list.add("brother  ");
+        list.add("brother-");
+        list.add("brother ");
         list.add("BROTHER");
         list.add("");
         list.add(" ");
         list.add("брат");
         list.add("брат123");
-        list.add("брат.,");
+        list.add("брат-");
         list.add("брат  ");
         list.add("БРАТ");
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-        prepareObject = new Prepare();
     }
 
     @Test (timeout = 1000)
@@ -38,10 +32,14 @@ public class PrepareTest {
         final List<String> listToBe = new ArrayList<>();
         listToBe.add("brother");
         listToBe.add("brother");
+        listToBe.add("brother");
+        listToBe.add("brother");
+        listToBe.add("брат");
+        listToBe.add("брат");
         listToBe.add("брат");
         listToBe.add("брат");
 
-        final  List<String> result = prepareObject.prepare(list);
+        final  List<String> result = Prepare.prepare(list);
 
         assertEquals(listToBe, result);
     }
