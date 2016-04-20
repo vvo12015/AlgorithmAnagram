bc22ad2311dd07b18a84e4e8aa84e0d5eed733a0
B1R1 <roman.burko@gmail.com>
2016-04-20 22:24:00 +0300

    FileReaderTest

---
+++ src/test/java/ua/com/anagram/MainTest.java
@@ -0,0 +1,16 @@
+package ua.com.anagram;
+
+public class MainTest {
+
+//    private Main mainObject;
+//    private List<String> validSourceText;
+//
+//    @Test
+//    public void testMain() throws Exception {
+//
+//
+//        assertEquals(expected, results);
+//
+//
+//    }
+}
--- src/test/java/ua/com/anagram/read/FileReaderTest.java
+++ src/test/java/ua/com/anagram/read/FileReaderTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,32 +16,31 @@ import static org.junit.Assert.assertEquals;
 public class FileReaderTest {
 
     private List<String> validList;
-//    private List<String> emptyList;
-    private String validFileNme;
-    private String invalidFileNme = " ";
+    private String validFileName;
+    private String emptyFileName;
 
     @Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
     public void setUp() throws IOException{
-        validFileNme = createAndFillFile();
+        validFileName = createAndFillFile();
         validList = getValidList();
-//        emptyList = getEmptyList();
+        emptyFileName = createEmptyFile();
     }
 
-    @Test (timeout = 1000)
+    @Test
     public void testRead() throws Exception {
-        FileReader fileReader = new FileReader(validFileNme);
+        FileReader fileReader = new FileReader(validFileName);
         final List<String> result = fileReader.read();
-
         assertEquals(validList, result);
     }
 
-    @Test(expected = FileNotFoundException.class)
-    public void testReadFileNotFoundException() throws Exception {
-        FileReader fileReader = new FileReader(invalidFileNme);
+    @Test
+    public void testReadEmptyFile() throws Exception {
+        FileReader fileReader = new FileReader(emptyFileName);
         final List<String> result = fileReader.read();
+        assertEquals(new ArrayList<String>().isEmpty(), result.isEmpty());
     }
 
     private List<String> getValidList() {
@@ -56,17 +54,16 @@ public class FileReaderTest {
         return list;
     }
 
-    private List<String> getEmptyList() {
-        List<String> list = new ArrayList<>();
-        list.add("");
-        return list;
-    }
-
     private String createAndFillFile() throws IOException {
-        File file = folder.newFile("tempfile.txt");
+        File file = folder.newFile("tempFile.txt");
         FileWriter writer = new FileWriter(file);
         writer.write("loveFile catFile tacFile actFile ovelFile readFile");
         writer.close();
-        return file.getName();
+        return file.getAbsolutePath();
+    }
+
+    private String createEmptyFile() throws IOException {
+        File file = folder.newFile("tempEmptyFile.txt");
+        return file.getAbsolutePath();
     }
 }
