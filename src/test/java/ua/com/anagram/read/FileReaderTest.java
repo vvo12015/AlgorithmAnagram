f6acec6506a23be185fc3c4f298f5ab241ce10a6
B1R1 <roman.burko@gmail.com>
2016-04-20 19:02:30 +0300

    Нерабочий FileReaderTest

--- src/test/java/ua/com/anagram/read/FileReaderTest.java
+++ src/test/java/ua/com/anagram/read/FileReaderTest.java
@@ -1,33 +1,72 @@
 package ua.com.anagram.read;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 public class FileReaderTest {
 
-    private final ArrayList<String> listResult = new ArrayList<String>();
+    private List<String> validList;
+//    private List<String> emptyList;
+    private String validFileNme;
+    private String invalidFileNme = " ";
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Before
-    public void setUpRead(){
-        listResult.add("loveFile");
-        listResult.add("catFile");
-        listResult.add("tacFile");
-        listResult.add("actFile");
-        listResult.add("ovelFile");
-        listResult.add("readFile");
+    public void setUp() throws IOException{
+        validFileNme = createAndFillFile();
+        validList = getValidList();
+//        emptyList = getEmptyList();
     }
 
-    @Test
-    public void read() throws Exception {
-        Reader reader = mock(Reader.class);
+    @Test (timeout = 1000)
+    public void testRead() throws Exception {
+        FileReader fileReader = new FileReader(validFileNme);
+        final List<String> result = fileReader.read();
 
-        when(reader.read()).thenReturn(listResult);
-
+        assertEquals(validList, result);
     }
 
+    @Test(expected = FileNotFoundException.class)
+    public void testReadFileNotFoundException() throws Exception {
+        FileReader fileReader = new FileReader(invalidFileNme);
+        final List<String> result = fileReader.read();
+    }
+
+    private List<String> getValidList() {
+        List<String> list = new ArrayList<>();
+        list.add("loveFile");
+        list.add("catFile");
+        list.add("tacFile");
+        list.add("actFile");
+        list.add("ovelFile");
+        list.add("readFile");
+        return list;
+    }
+
+    private List<String> getEmptyList() {
+        List<String> list = new ArrayList<>();
+        list.add("");
+        return list;
+    }
+
+    private String createAndFillFile() throws IOException {
+        File file = folder.newFile("tempfile.txt");
+        FileWriter writer = new FileWriter(file);
+        writer.write("loveFile catFile tacFile actFile ovelFile readFile");
+        writer.close();
+        return file.getName();
+    }
 }
