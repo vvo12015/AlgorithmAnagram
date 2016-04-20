cbb32805b7c263626d00d58ed2f610c7fe5e2e8b
vvo12 <vvo12015@gmail.com>
2016-04-21 00:05:59 +0300

    edit Kostja's photo in presentation

---
+++ src/test/java/ua/com/anagram/write/ConsoleWriterTest.java
@@ -0,0 +1,13 @@
+package ua.com.anagram.write;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConsoleWriterTest {
+    @Test
+    public void write() throws Exception {
+
+    }
+
+}
