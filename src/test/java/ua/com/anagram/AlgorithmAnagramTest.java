044b84b1fc25fe037344ff463ce93733ddc6468f
Tetiana Mokhnenko <Tetiana_Mokhnenko@epam.com>
2016-04-20 13:07:52 +0300

    some corrections

--- src/test/java/ua/com/anagram/AlgorithmAnagramTest.java
+++ src/test/java/ua/com/anagram/AlgorithmAnagramTest.java
@@ -1,6 +1,6 @@
 package ua.com.anagram;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.*;
@@ -9,9 +9,39 @@ import static org.junit.Assert.assertEquals;
 
 public class AlgorithmAnagramTest {
 
-    private static AlgorithmAnagram algorithmObject;
-    private static final List<String> list = new ArrayList<>();
-    static {
+    private AlgorithmAnagram algorithmObject;
+    private List<String> validSourceText;
+    private  Map<String, List<String>> validFindedAnagrams;
+    private List<String> inValidSourceText;
+    private List<String> inValidFindedAnograms;
+
+
+    @Before
+    public void setUp() throws Exception {
+        algorithmObject = new AlgorithmAnagram();
+        validSourceText = getPopulatedSourceText();
+        validFindedAnagrams = getValidAnagrams();
+
+    }
+
+    @Test (timeout = 1000)
+    public void testExecute() throws Exception {
+        final Map<String, List<String>>  actualResult = algorithmObject.execute(validSourceText);
+
+        assertEquals(validFindedAnagrams, actualResult);
+    }
+
+    private Map<String, List<String>> getValidAnagrams() {
+        final Map<String, List<String>> mapToBe = new HashMap<>();
+        mapToBe.put("кат", Arrays.asList("кат", "акт"));
+        mapToBe.put("мама", Arrays.asList("мама", "амам"));
+        mapToBe.put("рама", Collections.singletonList("рама"));
+        mapToBe.put("шина", Arrays.asList("шина", "ниша"));
+        return mapToBe;
+    }
+
+    private List<String> getPopulatedSourceText() {
+        List<String> list = new ArrayList<>();
         list.add("кат");
         list.add("мама");
         list.add("мама");
@@ -21,24 +51,7 @@ public class AlgorithmAnagramTest {
         list.add("шина");
         list.add("амам");
         list.add("ниша");
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-        algorithmObject = new AlgorithmAnagram();
-    }
-
-    @Test (timeout = 1000)
-    public void testExecute() throws Exception {
-        final Map<String, List<String>> mapToBe = new HashMap<>();
-        mapToBe.put("кат", Arrays.asList("кат", "акт"));
-        mapToBe.put("мама", Arrays.asList("мама", "амам"));
-        mapToBe.put("рама", Arrays.asList("рама"));
-        mapToBe.put("шина", Arrays.asList("шина", "ниша"));
-
-        final   Map<String, List<String>>  result = algorithmObject.execute(list);
-
-        assertEquals(mapToBe, result);
+        return list;
     }
 }
 // кат мама мама мама акт рама шина амам ниша
