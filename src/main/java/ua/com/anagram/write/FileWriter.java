1155e4e1e41dbee493ae0df1b0e9c31cb0441ef2
Petrov Konstantin <oku6er@gmail.com>
2016-04-21 16:36:17 +0600

    FileWriterTest now worked

--- src/main/java/ua/com/anagram/write/FileWriter.java
+++ src/main/java/ua/com/anagram/write/FileWriter.java
@@ -4,10 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +18,8 @@ public class FileWriter implements Writer {
         path = Paths.get(System.getProperty("user.home"), "\\AppData\\Local\\Temp", "anagrams.txt");
     }
 
-    public FileWriter(String fileName) {
-        path = Paths.get(System.getProperty("user.dir"), fileName);
+    public FileWriter(String filePath) {
+        path = Paths.get(filePath);
     }
 
     public void write(Map<String, List<String>> map) {
